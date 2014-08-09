11bc71996c79543ee549b0694e5f23f336b9fcbc
mrahman <rahmanww@gmail.com>
2014-08-09 14:23:03 -0400

    maven project

---
+++ src/main/java/com/cnn/news/App.java
@@ -0,0 +1,13 @@
+package com.cnn.news;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
